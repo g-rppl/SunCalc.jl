689aaa8c700e33acd261b68f9f6320f7c4bc74a0
g-rppl <g-rppl@posteo.de>
2023-09-06 11:16:21 +0200

    include docs in CI

--- src/sunPosition.jl
+++ src/sunPosition.jl
@@ -11,10 +11,10 @@ Calculate the sun position for the given time and location. Return a `NamedTuple
     
 Available variables:
 
-| Variable  | Description                                                                                                                 |
-| :-------- | :-------------------------------------------------------------------------------------------------------------------------- |
-|`altitude` | sun altitude above the horizon in radians, e.g. 0 at the horizon and π/2 at the zenith (straight over your head).           |
-|`azimuth`  | sun azimuth in radians (direction along the horizon, measured from south to west), e.g. 0 is south and π * 3/4 is northwest.|
+| Variable   | Description                                                                                                                 |
+| :--------- | :-------------------------------------------------------------------------------------------------------------------------- |
+| `altitude` | sun altitude above the horizon in radians, e.g. 0 at the horizon and π/2 at the zenith (straight over your head).           |
+| `azimuth`  | sun azimuth in radians (direction along the horizon, measured from south to west), e.g. 0 is south and π * 3/4 is northwest.|
 
 # Examples
 ```jldoctest
@@ -23,7 +23,10 @@ julia> getSunPosition(DateTime(2000, 07, 01, 12, 00, 00), 54, 9.0)
 (altitude = 1.021444013872015, azimuth = 0.23904867335099955)
 ```	
 """
-function getSunPosition(time::Union{DateTime,ZonedDateTime}, lat::Real, lon::Real;
+function getSunPosition(
+    time::Union{DateTime,ZonedDateTime},
+    lat::Real,
+    lon::Real;
     keep=[:altitude, :azimuth])
 
     available_var = [:altitude, :azimuth]
@@ -40,8 +43,11 @@ function getSunPosition(time::Union{DateTime,ZonedDateTime}, lat::Real, lon::Rea
     return data[keep]
 end
 
-function getSunPosition(time::Union{Vector{DateTime},Vector{ZonedDateTime}},
-    lat::Real, lon::Real; keep=[:altitude, :azimuth])
+function getSunPosition(
+    time::Union{Vector{DateTime},Vector{ZonedDateTime}},
+    lat::Real,
+    lon::Real;
+    keep=[:altitude, :azimuth])
 
     available_var = [:altitude, :azimuth]
 
